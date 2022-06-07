e82d280206c95c51426773dd7e08047645b5e33e
Sonia Prevost <so.prevost@gmail.com>
2022-06-07 10:09:42 +0200

    Check diffusible only if valid siret

--- app/models/artisan.rb
+++ app/models/artisan.rb
@@ -1,11 +1,15 @@
 class Artisan < ApplicationRecord
   validates :siret, length: { is: 14,
                               message: "Le champs doit comporter 14 chiffres pour un Siret" }
-  validate :entreprise_diffusible
+  validate :entreprise_diffusible, if: :check_siret
 
   def entreprise_diffusible
     artisan = ArtisanContact.get(siret: self.siret)
 
     errors.add(:base, "L'entreprise n'est pas diffusable") if artisan == "L'entreprise n'est pas diffusable"
+  end
+
+  def check_siret
+    self.siret.present? || self.siret == 14
   end
 end
