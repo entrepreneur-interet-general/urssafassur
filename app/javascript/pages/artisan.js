37338c7baf967fd454702aa08b9c4f7d88739024
Sonia Prevost <so.prevost@gmail.com>
2022-06-14 14:31:15 +0200

    Display/Hide Different Infos Div

---
+++ app/javascript/pages/artisan.js
@@ -0,0 +1,14 @@
+const btnDifferentInfos = document.querySelector('#different-infos');
+const btnSameInfo = document.querySelector('#same-infos');
+const differentInfosDiv = document.querySelector('#different-infos-div');
+
+function displayDifferentInfosDiv(event){
+  differentInfosDiv.classList.remove("d-none");
+}
+
+function hideDifferentInfosDiv(event){
+  differentInfosDiv.classList.add("d-none");
+}
+
+btnDifferentInfos.addEventListener('click', displayDifferentInfosDiv);
+btnSameInfo.addEventListener('click', hideDifferentInfosDiv);
